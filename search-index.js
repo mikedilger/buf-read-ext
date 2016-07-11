9a6be28cb9177bcd134a5f8003c0340595fde51d
Mike Dilger <mike@optcomp.nz>
2016-07-12 10:48:26 +1200

    doc update for 0.2

--- search-index.js
+++ search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex["buf_read_ext"] = {"doc":"","items":[[8,"BufReadExt","buf_read_ext","Extends any type that implements BufRead with a stream_until_token() function.",null,null],[11,"stream_until_token","","Streams all bytes to `out` until the `token` delimiter is reached.",0,null]],"paths":[[8,"BufReadExt"]]};
+searchIndex["buf_read_ext"] = {"doc":"","items":[[8,"BufReadExt","buf_read_ext","Extends any type that implements BufRead with a stream_until_token() function.",null,null],[11,"stream_until_token","","Streams all bytes to `out` until the `token` delimiter or EOF is reached.",0,null]],"paths":[[8,"BufReadExt"]]};
 initSearch(searchIndex);
